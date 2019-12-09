f5b9838a85e732bbdd874e81153ddcb233466da2
BryanLutz <Bryan.Lutz@united.com>
2019-12-09 13:38:44 -0500

    Read lines from the file

---
+++ AdventOfCode2019/Day1/Day1.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode2019.Day1
+{
+    public static class Day1
+    {
+        static void Main(string[] args)
+        {
+            int lineCount = 0;
+            string line;
+
+            var file = new System.IO.StreamReader(@"Day1/Day1-input.txt");
+            while((line = file.ReadLine()) != null)
+            {
+                System.Console.WriteLine(line);
+                lineCount++;
+            }
+
+            file.Close();
+            System.Console.WriteLine($"There were {lineCount} lines.");
+            System.Console.ReadLine();
+        }
+    }
+}
