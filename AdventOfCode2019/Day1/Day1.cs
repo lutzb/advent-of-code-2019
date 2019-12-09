9f70daed51213e9e69ce8e4842ddd38e64c68db1
BryanLutz <Bryan.Lutz@united.com>
2019-12-09 14:15:37 -0500

    Day 1 complete

--- AdventOfCode2019/Day1/Day1.cs
+++ AdventOfCode2019/Day1/Day1.cs
@@ -1,22 +1,37 @@
 ﻿namespace AdventOfCode2019.Day1
 {
+    using System;
+
     public static class Day1
     {
         static void Main(string[] args)
         {
-            int lineCount = 0;
+            int totalFuel = 0;
             string line;
 
             var file = new System.IO.StreamReader(@"Day1/Day1-input.txt");
             while((line = file.ReadLine()) != null)
             {
                 System.Console.WriteLine(line);
-                lineCount++;
+                var currentFuel = DoCalculation(Int32.Parse(line));
+                totalFuel += currentFuel;
+
+                var nextFuel = DoCalculation(currentFuel);
+                while (nextFuel > 0)
+                {
+                    totalFuel += nextFuel;
+                    nextFuel = DoCalculation(nextFuel);
+                }
             }
 
             file.Close();
-            System.Console.WriteLine($"There were {lineCount} lines.");
+            System.Console.WriteLine(totalFuel);
             System.Console.ReadLine();
+        }
+
+        private static int DoCalculation(int i)
+        {
+            return Convert.ToInt32(Math.Floor((decimal) (i / 3))) - 2;
         }
     }
 }
